1715e47d60c3404e1857364b7e1a68b5cc13bb24
Aadhitya A <aadhitya864@gmail.com>
2022-02-06 21:16:23 +0530

    Init empty Swing form

---
+++ dist/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"kite","c":"Appl","l":"Appl()","u":"%3Cinit%3E()"},{"p":"kite","c":"Kite","l":"Kite()","u":"%3Cinit%3E()"},{"p":"kite","c":"Appl","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"kite","c":"Kite","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"kite","l":"Appl"},{"p":"kite","l":"Kite"}];updateSearchResults();
